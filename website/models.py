6d6d8e2b429648bc31d750210711b1308212c618
Krzysztof F <krzysztoff0423@gmail.com>
2024-01-10 21:01:36 +0100

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,5 +1,12 @@
 from website import base
 from flask_login import UserMixin
+from sqlalchemy.sql import func 
+
+class model(base.Model):
+    id = base.Column(base.Integer, primery_key = True)
+    text = base.Column(base.String(50))
+    date = base.Column(base.DateTime(timezone=True), default=func.now())
+    user_id = base.Column(base.Integer, base.ForeignKey('user.id'))
 
 class user(base.Model, UserMixin):
     id = base.Column(base.Integer, primery_key = True)
