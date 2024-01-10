f350c54c6540102c0ee01387491aabbddbd4ac9d
Krzysztof F <krzysztoff0423@gmail.com>
2024-01-10 21:12:00 +0100

    changrs

--- website/__init__.py
+++ website/__init__.py
@@ -1,18 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 base = SQLAlchemy()
 DB_NAME = "baza.db" 
 
 
-
-
-
-
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Forek42'
-    app.config['SQLAlchemy_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     base.init_app(app)
 
     from .views import views
@@ -21,4 +18,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User, Info
+    crt_db(app)
+
     return app
+
+
+def crt_db(app):
+        if not path.exists('website/' + DB_NAME):
+              base.create_all(app=app)
+              
--- website/models.py
+++ website/models.py
@@ -2,16 +2,16 @@ from website import base
 from flask_login import UserMixin
 from sqlalchemy.sql import func 
 
-class model(base.Model):
+class Info(base.Model):
     id = base.Column(base.Integer, primery_key = True)
     text = base.Column(base.String(50))
     date = base.Column(base.DateTime(timezone=True), default=func.now())
     user_id = base.Column(base.Integer, base.ForeignKey('user.id'))
 
-class user(base.Model, UserMixin):
+class User(base.Model, UserMixin):
     id = base.Column(base.Integer, primery_key = True)
     email = base.Column(base.String(25), unique = True)
     password = base.Column(base.String(25))
     nickname = base.Column(base.String(25))
-
+    info = base.relationship('Info')
 
